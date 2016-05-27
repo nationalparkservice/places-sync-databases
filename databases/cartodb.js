9677b32200ab42698abf8ce04fe232beb076a9b8
Jim McAndrew <jim@loc8.us>
2016-05-27 09:18:16 -0600

    Use cartdb js instead of superagent

--- databases/cartodb.js
+++ databases/cartodb.js
@@ -1,7 +1,7 @@
 var Promise = require('bluebird');
-var superagent = require('superagent');
 var fandlebars = require('fandlebars');
 var tools = require('jm-tools');
+var CartoDB = require('cartodb');
 
 var format = function (output) {
   // TODO: Clean up the output from this so it's similar to all other outputs
@@ -22,7 +22,7 @@ var parameterize = function (param, type) {
     param = param.toString();
   }
   var returnValue = "convert_from(decode('";
-  // type = type || tools.getDataType(param);
+  // type = type || tools.getDataType(param)
   if (type !== 'text') {
     returnValue = "'" + param.replace(/\'/g, "''") + (type ? "'::" + type : "'");
   } else {
@@ -46,33 +46,48 @@ var parameterizeQuery = function (query, params, columns) {
   return fandlebars(query, params);
 };
 
-var sendRequest = function (requestPath, cleanedSql, returnRaw, apiKey, attempts) {
+var sendRequest = function (sql, vars, returnRaw, account, apiKey, attempts) {
+  attempts = attempts || 0;
   var maxAttempts = 5;
   return new Promise(function (fulfill, reject) {
-    superagent.post(requestPath)
-      .set('Content-Type', 'application/json')
-      .set('Accept', 'application/json')
-      .send({
-        'q': cleanedSql,
-        'api_key': apiKey // connectionConfig.connection.apiKey
+    var cartoDatabase = new CartoDB.SQL({
+      user: account,
+      api_key: apiKey
+    });
+    cartoDatabase.execute(sql, vars, {
+      format: 'json'
+    })
+      .done(function (response) {
+        fulfill(returnRaw ? response : format(response));
       })
-      .end(function (err, response) {
-        if (err || response.error) {
-          if (response.error && response.error.text && response.error.text.match('query_wait_timeout') && attempts <= maxAttempts) {
-            // Time out error, so we can try again with a wait
-            console.log('Error, so trying again, on attempt:', attempts, 'timeout', 1000 + (attempts * 500));
-            setTimeout(function () {
-              sendRequest(requestPath, cleanedSql, returnRaw, apiKey, attempts + 1)
-                .then(fulfill)
-                .catch(reject);
-            }, 1000 + (attempts * 500));
-          } else {
-            reject(new Error(JSON.stringify(err || response, null, 2)));
-          }
-        } else {
-          fulfill(returnRaw ? response : format(response));
-        }
+      .error(function (err) {
+        reject(new Error(JSON.stringify(err, null, 2)));
       });
+  /*    superagent.post(requestPath)
+        .set('Content-Type', 'application/json')
+        .set('Accept', 'application/json')
+        .send({
+          'q': cleanedSql,
+          'api_key': apiKey // connectionConfig.connection.apiKey
+        })
+        .end(function (err, response) {
+          if (err || response.error) {
+            if (response.error && response.error.text && response.error.text.match('query_wait_timeout') && attempts <= maxAttempts) {
+              // Time out error, so we can try again with a wait
+              console.log('Error, so trying again, on attempt:', attempts, 'timeout', 1000 + (attempts * 500))
+              setTimeout(function () {
+                sendRequest(sql, vars, returnRaw, account, apiKey, attempts + 1)
+                  .then(fulfill)
+                  .catch(reject)
+              }, 1000 + (attempts * 500))
+            } else {
+              reject(new Error(JSON.stringify(err || response, null, 2)))
+            }
+          } else {
+            fulfill(returnRaw ? response : format(response))
+          }
+        })
+        */
   });
 };
 
@@ -81,10 +96,10 @@ module.exports = function (connectionConfig) {
     query: function (query, params, returnRaw, columns) {
       return new Promise(function (fulfill, reject) {
         var cleanedSql = parameterizeQuery(query, params, columns);
-        var requestPath = 'https://' + connectionConfig.connection.account + '.cartodb.com/api/v2/sql';
+        // var requestPath = 'https://' + connectionConfig.connection.account + '.cartodb.com/api/v2/sql'
 
         if (cleanedSql.length > 5) {
-          sendRequest(requestPath, cleanedSql, returnRaw, connectionConfig.connection.apiKey, 0)
+          sendRequest(cleanedSql, returnRaw, connectionConfig.connection.account, connectionConfig.connection.apiKey)
             .then(fulfill)
             .catch(reject);
         } else {
